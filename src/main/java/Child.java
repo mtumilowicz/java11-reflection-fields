6fdec6ca99e9e96136efeaed5f38506eab617ab0
mtumilowicz <michal.tumilowicz01@gmail.com>
2019-02-01 21:46:54 +0100

    classes

---
+++ src/main/java/Child.java
@@ -0,0 +1,15 @@
+/**
+ * Created by mtumilowicz on 2019-02-01.
+ */
+class Child extends Parent {
+    
+    private int privateChildField;
+    String packageChildPrivateField;
+    protected Object protectedChildField;
+    public int publicChildField;
+    
+}
+
+interface ChildInterface {
+    String ChildInterfaceField = "";
+}
---
+++ src/main/java/Parent.java
@@ -0,0 +1,13 @@
+/**
+ * Created by mtumilowicz on 2019-02-01.
+ */
+class Parent {
+    private int privateParentField;
+    String packageParentPrivateField;
+    protected Object protectedParentField;
+    public int publicParentField;
+}
+
+interface ParentInterface {
+    String ParentInterfaceField = "";
+}
